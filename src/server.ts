2a17c16d1b0cfd9b3fa04b6ea7db3ca7a3e5bf50
Willian Tavares <willianliketavares@gmail.com>
2020-05-03 15:26:05 -0300

    finalized, tests done

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes/index';
 import AppError from './errors/AppError';
@@ -10,6 +11,7 @@ import './database/index';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
